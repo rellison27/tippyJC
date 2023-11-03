155b236c119c160b5ba78920247c9f9a3b7380be
Rashaun Ellison <rellison27@gmail.com>
2023-11-02 20:41:53 -0400

    added round up feature

--- app/src/main/java/com/rellidev/tippy/MainActivity.kt
+++ app/src/main/java/com/rellidev/tippy/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
@@ -49,10 +50,11 @@ fun Tippy()
 {
     var amountInput by remember { mutableStateOf("") }
     var tipInput by remember { mutableStateOf("") }
+    var isRoundingUp by remember { mutableStateOf(false) }
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0
     val tipPercentage = tipInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount, tipPercentage)
+    val tip = calculateTip(amount, tipPercentage, isRoundingUp)
 
     Column(
         modifier = Modifier
@@ -93,6 +95,10 @@ fun Tippy()
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
         )
+        RoundTipRow(
+            isRoundingUp = isRoundingUp,
+            onCheckedChanged = { isRoundingUp = it }
+        )
         Text(
             text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
@@ -118,14 +124,40 @@ fun EditNumberField(
         keyboardOptions = keyboardOptions
     )
 }
+@Composable
+fun RoundTipRow(
+    isRoundingUp: Boolean,
+    onCheckedChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(6.dp)
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically,
+//        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Text(text = stringResource(id = R.string.round_up_tip))
+        Switch(
+            checked = isRoundingUp,
+            onCheckedChange = onCheckedChanged,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+        )
+    }
+}
 
 /**
  * Calculates the tip based on the user input and format the tip amount
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if(roundUp) tip = kotlin.math.ceil(tip)
+
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
